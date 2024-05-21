b035f13457a3654878afcbcf6dbc9d2699b1b896
Junior Alves <jrnalves@gmail.com>
2024-05-21 09:44:45 -0300

    feat:Criar botão de "criar conta"

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 export default function App() {
   return (
     <View>
-      <Image source={require("@/src/assets/imagens/logo.png")} alt="Logo Bora rachar" style={{ width: 200, height: 80 }} />
+      <Image
+        source={require('@/src/assets/imagens/logo.png')}
+        alt="Logo Bora rachar"
+        style={{ width: 200, height: 80 }}
+      />
+
+      <View>
+        <TouchableOpacity>
+          <Text>Criar minha conta</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
