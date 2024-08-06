e8ce9069e15800d3cfc42c85c72a9a0cbccbc6a6
Junior Alves <jrnalves@gmail.com>
2024-08-06 18:13:06 -0300

    feat: Adicionar fonte Inter com peso de 400, 500, 600 e 700

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native'
+import { theme } from '../theme'
 
 export default function Index() {
   return (
@@ -9,7 +10,9 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontFamily: theme.fontFamily.InterRegular }}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   )
 }
---
+++ src/theme/font-family.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+  InterRegular: 'InterRegular',
+  InterMedium: 'InterMedium',
+  InterSemiBold: 'InterSemiBold',
+  InterBold: 'InterBold',
+}
