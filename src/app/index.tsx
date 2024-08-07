a52f77bb0a0e7e593c0c1944e909fa4825f0ea12
Junior Alves <jrnalves@gmail.com>
2024-08-07 17:23:20 -0300

    feat: adiconar a Logo do bora rachar em svg no projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,15 @@
 import { Text, View } from 'react-native'
 import { theme } from '../theme'
 
+import Logo from '@/src/assets/logo.svg'
+
 export default function Index() {
   return (
     <View>
       <Text style={{ fontFamily: theme.fontFamily.InterRegular }}>
         Edit app/index.tsx to edit this screen.
       </Text>
+      <Logo width={196} height={80} />
     </View>
   )
 }
